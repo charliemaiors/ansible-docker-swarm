ecd147b1ccb3f2727b4f3d425055c95672e65ec0
Pasquale Carlo Maiorano Picone <charliemaiors@gmail.com>
2017-03-21 17:40:34 +0000

    always windows 10 bash...

--- start-installation.sh
+++ start-installation.sh
@@ -4,4 +4,4 @@ eval `ssh-agent -s`
 ssh-add /home/carlo/.ssh/trystack-maior.pem
 
 ansible-playbook master.yml
-ssh trystack 'export ANSIBLE_HOST_KEY_CHECKING=False; eval ssh-agent -s; ssh-add [.ssh/trystack-maior.pem; ansible-playbook worker.yml'
+ssh trystack 'export ANSIBLE_HOST_KEY_CHECKING=False; eval `ssh-agent -s`; ssh-add ~/.ssh/trystack-maior.pem; ansible-playbook worker.yml'
