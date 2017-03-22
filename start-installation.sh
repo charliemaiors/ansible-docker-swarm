ded69db2a6a4b1a0c7fda114e6f35138f74a786a
charliemaiors <charliemaiors@gmail.com>
2017-03-22 14:34:28 +0100

    started refactoring of installation script in order to install also ansible and use parametrize everything

--- start-installation.sh
+++ start-installation.sh
@@ -1,5 +1,53 @@
 #! /bin/bash
 
+isnumber='^[0-9]+$'
+
+check_binary () {
+  echo -n " * Checking for '$1' ... "
+  if command -v $1 >/dev/null 2>&1; then
+     echo "OK"
+     return 0
+  else
+     echo >&2 "FAILED"
+     return 1
+  fi
+}
+
+#Execute command?
+_ex='sh -c'
+if [ "${USER}" != "root" ]; then
+    if check_binary sudo; then
+        _ex='sudo -E sh -c'
+    elif check_binary su; then
+        _ex='su -c'
+    fi
+fi
+
+#Retrieving package manager
+_pkgmgr=''
+if check_binary apt-get; then #Ubuntu
+    _pkgmgr='apt-get'
+elif check_binary yum; then #CentOS/RedHat
+    _pkgmgr='yum'
+elif check_binary aptitue; then #Debian old version?
+    _pkgmgr='aptitude'
+fi
+
+#Installing ansible if is not present
+
+if !check_binary ansible; then
+  $_ex $_pkgmgr 'update -y' #if is update -y on ubuntu does not matter
+  $_ex $_pkgmgr 'install -y ansible'
+fi
+
+read -p "What is "
+
+read -p "how many workers you have? " host_number
+
+if ! [[ $host_number = $isnumber ]] ; then
+   echo "error: Not an integer number" >&2; exit 1
+fi
+
 read -p "write the ip address of the first slave: " host_ip
 sed -i "s|changeme1 ansible_connection=ssh ansible_user=centos ansible_ssh_private_key_file=trystack-maior.pem|$host_ip ansible_connection=ssh ansible_user=centos ansible_ssh_private_key_file=trystack-maior.pem|g" hosts
 
