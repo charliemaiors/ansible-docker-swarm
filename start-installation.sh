3236533140cfdb8c5a2d02567394b683a0361a93
charliemaiors <charliemaiors@gmail.com>
2017-03-22 12:09:10 +0100

    fixed installation now hopefully works

--- start-installation.sh
+++ start-installation.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 
-read -p "write the ip address of first slave: " host_ip
-sed -i "s/changeme-1*/$host_ip*/g" hosts
+read -p "write the ip address of the first slave: " host_ip
+sed -i "s|changeme1 ansible_connection=ssh ansible_user=centos ansible_ssh_private_key_file=trystack-maior.pem|$host_ip ansible_connection=ssh ansible_user=centos ansible_ssh_private_key_file=trystack-maior.pem|g" hosts
 
+read -p "write the ip address of the second slave: " host_ip
+sed -i "s|changeme2 ansible_connection=ssh ansible_user=centos ansible_ssh_private_key_file=trystack-maior.pem|$host_ip ansible_connection=ssh ansible_user=centos ansible_ssh_private_key_file=trystack-maior.pem|g" hosts
 
 eval `ssh-agent -s`
 ssh-add /home/carlo/.ssh/trystack-maior.pem
 
 ansible-playbook master.yml
-ssh trystack 'export ANSIBLE_HOST_KEY_CHECKING=False; eval `ssh-agent -s`; ssh-add ~/.ssh/trystack-maior.pem; ansible-playbook worker.yml'
+ssh trystack bash -c 'export ANSIBLE_SSH_ARGS=UserKnownHostsFile=/dev/null; export ANSIBLE_HOST_KEY_CHECKING=False; eval `ssh-agent -s`; ssh-add ~/.ssh/trystack-maior.pem; ansible-playbook worker.yml'
